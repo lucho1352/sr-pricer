a58124bf6858e47f614359d4f993e28406d76d30
luisao <lucho1352@gmail.com>
2023-11-29 06:31:38 -0500

    Add: Initial set up

---
+++ src/main/java/com/inditex/pricer/SrPricerApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.pricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrPricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrPricerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/pricer/SrPricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.pricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrPricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
