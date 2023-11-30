62074f30ad620b9c86b504c7e2c3c0a35d76663b
luisao <lucho1352@gmail.com>
2023-11-30 05:37:24 -0500

    Add: Added swagger integration

---
+++ Readme.md
@@ -0,0 +1,7 @@
+### Swagger
+
+In next link you will be able tu explore the swagger locally, but remember to change the 
+localhost and port for the one that you are exposing, in order to be able to read the swagger
+from your corresponding environment
+
+http://localhost:8080/swagger-ui/index.html
--- src/main/java/com/inditex/pricer/controller/PriceController.java
+++ src/main/java/com/inditex/pricer/controller/PriceController.java
@@ -4,6 +4,12 @@ package com.inditex.pricer.controller;
 import com.inditex.pricer.dto.PriceRequest;
 import com.inditex.pricer.dto.PriceResponse;
 import com.inditex.pricer.service.PriceCalculator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Validated
 @RestController
+@Tag(name = "Prices")
 @RequestMapping("/v1/price")
 @AllArgsConstructor
 public class PriceController {
@@ -23,7 +30,15 @@ public class PriceController {
     private final PriceCalculator priceCalculator;
 
     @GetMapping
+    @Operation(summary = "Gets price by brand id, product id and a date time")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Price found",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PriceResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Price not found", content = @Content) })
     public PriceResponse determinePrice(@RequestBody @Valid PriceRequest priceRequest){
+
         return priceCalculator.calculatePrice(priceRequest);
     }
 }
